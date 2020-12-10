ef304b34239238823b4676c6d0f24413b59e62c3
Build <support@mindpowered.dev>
2020-12-10 06:24:13 +0000

    Version 0.0.1

---
+++ demowrapper/IPTools.cs
@@ -0,0 +1,119 @@
+/**
+ * Copyright Mind Powered Corporation 2020
+ * 
+ * https://mindpowered.dev/
+ */
+
+/**
+ * IP Address Conversion and CIDR calculator
+ */
+namespace wrappers {
+	public class IPTools
+
+		/**
+		 * Convert a subnet mask to CIDR notation
+		 * @param subnet_mask A subnet mask eg. 255.255.0.0
+		 * @return {number} The x value.
+		*/
+		SubnetMaskToCIDR(subnet_mask)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [subnet_mask];
+			//TODO: C# let ret = bus.call('IPTools.SubnetMaskToCIDR', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Given an IP address or IP address with CIDR notation, provides information about an IP address range
+		 * @param ip_addr An IP address with or without CIDR notation
+		 * @return {number} The x value.
+		*/
+		CalcCIDR(ip_addr)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip_addr];
+			//TODO: C# let ret = bus.call('IPTools.CalcCIDR', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Convert an IP address to a Decimal representation
+		 * @param ip_addr An IP address
+		 * @return {number} The x value.
+		*/
+		IPToDecimal(ip_addr)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip_addr];
+			//TODO: C# let ret = bus.call('IPTools.IPToDecimal', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Convert from a Decimal representationan of an IP address
+		 * @param ip4_number Decimal representation of an IP address
+		 * @return {number} The x value.
+		*/
+		IP4FromDecimal(ip4_number)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip4_number];
+			//TODO: C# let ret = bus.call('IPTools.IP4FromDecimal', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Convert from a Decimal representationan of an IP address
+		 * @param ip6_number Decimal representation of an IP address
+		 * @return {number} The x value.
+		*/
+		IP6FromDecimal(ip6_number)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip6_number];
+			//TODO: C# let ret = bus.call('IPTools.IP6FromDecimal', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Convert an IPv4 address to an IPv6 address
+		 * @param ip_addr An IPv4 address
+		 * @return {number} The x value.
+		*/
+		IPv4ToIPv6(ip_addr)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip_addr];
+			//TODO: C# let ret = bus.call('IPTools.IPv4ToIPv6', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Expand the zeros in an IPv6 address
+		 * @param ip6_addr An IPv6 address
+		 * @return {number} The x value.
+		*/
+		ExpandIPv6(ip6_addr)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip6_addr];
+			//TODO: C# let ret = bus.call('IPTools.ExpandIPv6', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Compress the zeros in an IPv6 address
+		 * @param ip6_addr An IPv6 address
+		 * @return {number} The x value.
+		*/
+		CompressIPv6(ip6_addr)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
+			//TODO: C# let args = [ip6_addr];
+			//TODO: C# let ret = bus.call('IPTools.CompressIPv6', args);
+			//TODO: C# return ret;
+		}
+
+	}
+}
+
